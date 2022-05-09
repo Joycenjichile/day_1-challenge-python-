c14565a2d32ed7417d8634462ae58c8a0f6cbef0
joycenjichile <joycenkimalantoi@gmail.com>
2022-05-09 19:53:31 +0300

    first commit

---
+++ helloworld.py
@@ -0,0 +1,44 @@
+import numbers
+from tokenize import Double, Floatnumber, String
+from typing import List
+from xmlrpc.client import Boolean
+
+
+print(3+4)
+print(3-4)
+print(3*4)
+print(3%4)
+print(3/4)
+print(3**4)
+print(3//4)
+print("JOY")
+print("NJICHILE")
+print("Tanzania")
+print("I am enjoying 30 days of code")
+print(type(10)) 
+print(type(9.8))  
+print(type(3.14))  
+print(type(4.4j))
+#print(type("Asabener","Python","Finland"))
+#print(type("Joyce"))
+#print(type("Njichile"))     
+#print(type("Kenya"))
+X=12
+print(X)
+y=2.4
+print(y)
+z=2,6j
+print(z)
+c=("owiti")
+print(c)
+v=2.44
+print(v)
+b=[10,26,35,32,12]
+print(b)
+g=("beans","mango","salt")
+print(g)
+x={1.4,3,6,8,9}
+print(x)
+y={"joy","cudra"}
+print(y)
+
